bf38a83b32b0d3e24d638fab2d793dfe61928242
Terrence Park <trjunk@parkvue.com>
2021-03-01 01:15:48 -0800

    Add error meter options
    * Scaling
    * Tick life
    * Bottom arrow sensitivity

--- AdofaiTweaks/Tweaks/JudgmentVisuals/JudgmentVisualsTweak.cs
+++ AdofaiTweaks/Tweaks/JudgmentVisuals/JudgmentVisualsTweak.cs
@@ -24,7 +24,81 @@ namespace AdofaiTweaks.Tweaks.JudgmentVisuals
 
         public override void OnSettingsGUI() {
             Settings.ShowHitErrorMeter =
-                GUILayout.Toggle(Settings.ShowHitErrorMeter, "Show hit error meter");
+                GUILayout.Toggle(
+                    Settings.ShowHitErrorMeter,
+                    TweakStrings.Get(TranslationKeys.JudgmentVisuals.SHOW_HIT_ERROR_METER));
+
+            if (Settings.ShowHitErrorMeter) {
+                GUILayout.BeginHorizontal();
+                GUILayout.Space(20f);
+                GUILayout.BeginVertical();
+
+                // Scale slider
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(
+                    TweakStrings.Get(TranslationKeys.JudgmentVisuals.ERROR_METER_SCALE));
+                GUILayout.Space(4f);
+                float newScale =
+                    GUILayout.HorizontalSlider(
+                        Settings.ErrorMeterScale, 0.25f, 4f, GUILayout.Width(200f));
+                newScale *= 4;
+                newScale = Mathf.Round(newScale);
+                newScale /= 4;
+                if (Settings.ErrorMeterScale != newScale) {
+                    Settings.ErrorMeterScale = newScale;
+                    HitErrorMeter.Instance.Scale = newScale;
+                }
+                GUILayout.Space(8f);
+                GUILayout.Label(Settings.ErrorMeterScale + "x");
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                // Tick life slider
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(
+                    TweakStrings.Get(TranslationKeys.JudgmentVisuals.ERROR_METER_TICK_LIFE));
+                GUILayout.Space(4f);
+                float newLife =
+                    GUILayout.HorizontalSlider(
+                        Settings.ErrorMeterTickLife, 1f, 10f, GUILayout.Width(200f));
+                newLife = Mathf.Round(newLife);
+                if (Settings.ErrorMeterTickLife != newLife) {
+                    Settings.ErrorMeterTickLife = newLife;
+                }
+                GUILayout.Space(8f);
+                GUILayout.Label(
+                    TweakStrings.Get(
+                        TranslationKeys.JudgmentVisuals.ERROR_METER_TICK_SECONDS,
+                        Settings.ErrorMeterTickLife));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                // Sensitivity slider
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(
+                    TweakStrings.Get(TranslationKeys.JudgmentVisuals.ERROR_METER_SENSITIVITY));
+                GUILayout.Space(16f);
+                GUILayout.Label(
+                    TweakStrings.Get(TranslationKeys.JudgmentVisuals.ERROR_METER_MORE_STABLE));
+                GUILayout.Space(8f);
+                float newSens =
+                    GUILayout.HorizontalSlider(
+                        Settings.ErrorMeterSensitivity, 0.05f, 0.5f, GUILayout.Width(200f));
+                newSens *= 20;
+                newSens = Mathf.Round(newSens);
+                newSens /= 20;
+                if (Settings.ErrorMeterSensitivity != newSens) {
+                    Settings.ErrorMeterSensitivity = newSens;
+                }
+                GUILayout.Space(8f);
+                GUILayout.Label(
+                    TweakStrings.Get(TranslationKeys.JudgmentVisuals.ERROR_METER_LESS_STABLE));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                GUILayout.EndVertical();
+                GUILayout.EndHorizontal();
+            }
         }
 
         public override void OnUpdate(float deltaTime) {
