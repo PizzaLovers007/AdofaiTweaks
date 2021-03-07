ff8b9f172dc39a208a69cc3980c7636a2c346dc0
Terrence Park <trjunk@parkvue.com>
2021-03-07 07:23:58 -0800

    Quick fix broken styling, need to properly fix later

--- AdofaiTweaks/Tweaks/PlanetColor/PlanetColorTweak.cs
+++ AdofaiTweaks/Tweaks/PlanetColor/PlanetColorTweak.cs
@@ -77,12 +77,14 @@ namespace AdofaiTweaks.Tweaks.PlanetColor
             GUILayout.BeginHorizontal();
             GUILayout.Label(
                 TweakStrings.Get(TranslationKeys.PlanetColor.BODY_HEX), GUILayout.Width(90f));
-            newHex = GUILayout.TextField(oldHex, GUILayout.Width(100f));
             GUILayout.FlexibleSpace();
+            newHex = GUILayout.TextField(oldHex, GUILayout.Width(100f));
+            GUILayout.Space(50f);
             GUILayout.Label(
                 TweakStrings.Get(TranslationKeys.PlanetColor.TAIL_HEX), GUILayout.Width(90f));
-            newTailHex = GUILayout.TextField(oldTailHex, GUILayout.Width(100f));
             GUILayout.FlexibleSpace();
+            newTailHex = GUILayout.TextField(oldTailHex, GUILayout.Width(100f));
+            GUILayout.Space(50f);
             GUILayout.EndHorizontal();
             if (oldHex != newHex && ColorUtility.TryParseHtmlString(newHex, out Color newColor)) {
                 Settings.Color1 = newColor;
@@ -144,12 +146,14 @@ namespace AdofaiTweaks.Tweaks.PlanetColor
             GUILayout.BeginHorizontal();
             GUILayout.Label(
                 TweakStrings.Get(TranslationKeys.PlanetColor.BODY_HEX), GUILayout.Width(90f));
-            newHex = GUILayout.TextField(oldHex, GUILayout.Width(100f));
             GUILayout.FlexibleSpace();
+            newHex = GUILayout.TextField(oldHex, GUILayout.Width(100f));
+            GUILayout.Space(50f);
             GUILayout.Label(
                 TweakStrings.Get(TranslationKeys.PlanetColor.TAIL_HEX), GUILayout.Width(90f));
-            newTailHex = GUILayout.TextField(oldTailHex, GUILayout.Width(100f));
             GUILayout.FlexibleSpace();
+            newTailHex = GUILayout.TextField(oldTailHex, GUILayout.Width(100f));
+            GUILayout.Space(50f);
             GUILayout.EndHorizontal();
             if (oldHex != newHex && ColorUtility.TryParseHtmlString(newHex, out newColor)) {
                 Settings.Color2 = newColor;
