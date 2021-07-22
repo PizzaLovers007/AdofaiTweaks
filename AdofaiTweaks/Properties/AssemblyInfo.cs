344147d74460fe27c5a9534ebb5dbee159d12a87
CrackThrough <59867573+CrackThrough@users.noreply.github.com>
2021-07-22 11:06:29 +0900

    Edit AssemblyInfo to mark the assembly as a melonloader mod

--- AdofaiTweaks/Properties/AssemblyInfo.cs
+++ AdofaiTweaks/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using MelonLoader;
 
 // General Information about an assembly is controlled through the following set
 // of attributes. Change these attribute values to modify the information
@@ -29,3 +30,10 @@ using System.Runtime.InteropServices;
 // Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// MelonLoader Mod Informations
+[assembly: MelonInfo(typeof(AdofaiTweaks.AdofaiTweaks), "AdofaiTweaks", "2.4.0", "PizzaLovers007 & CrackThrough")]
+[assembly: MelonGame("7th Beat Games", "A Dance of Fire and Ice")]
+
+// Do not patch everything but allow me to safely choose which patch to patch
+[assembly: HarmonyDontPatchAll]
