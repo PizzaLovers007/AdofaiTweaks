ce8f48f158b20aaf67ef59966ae69786b0fff99b
PizzaLovers007 <trex@parkvue.com>
2021-07-27 02:20:22 -0700

    Fix issues in debug build config

--- umm_make_release.bat
+++ umm_make_release.bat
@@ -1,5 +1,6 @@
-msbuild /p:Configuration=Release
-cd AdofaiTweaks.Generator/bin/Release
+set config=Release
+msbuild /p:Configuration=%config%
+cd AdofaiTweaks.Generator/bin/%config%
 .\AdofaiTweaks.Generator.exe
 cd ../../..
 set /p version=<VERSION.txt
@@ -8,12 +9,12 @@ cd tmp
 mkdir AdofaiTweaks
 cp ../Info.json AdofaiTweaks
 cp ../UnityProject/Assets/AssetBundles/adofai_tweaks.assets AdofaiTweaks
-cp ../AdofaiTweaks/bin/Release/AdofaiTweaks.dll AdofaiTweaks
-cp ../AdofaiTweaks/bin/Release/DotNetZip.dll AdofaiTweaks
-cp ../AdofaiTweaks/bin/Release/LiteDB.dll AdofaiTweaks
-cp ../AdofaiTweaks.Translation/bin/Release/AdofaiTweaks.Translation.dll AdofaiTweaks
-cp ../AdofaiTweaks.Generator/bin/Release/TweakStrings.db AdofaiTweaks
-cp ../AdofaiTweaks.Generator/bin/Release/AdofaiTweaks.Strings.dll AdofaiTweaks
+cp ../AdofaiTweaks/bin/%config%/AdofaiTweaks.dll AdofaiTweaks
+cp ../AdofaiTweaks/bin/%config%/DotNetZip.dll AdofaiTweaks
+cp ../AdofaiTweaks/bin/%config%/LiteDB.dll AdofaiTweaks
+cp ../AdofaiTweaks.Translation/bin/%config%/AdofaiTweaks.Translation.dll AdofaiTweaks
+cp ../AdofaiTweaks.Generator/bin/%config%/TweakStrings.db AdofaiTweaks
+cp ../AdofaiTweaks.Generator/bin/%config%/AdofaiTweaks.Strings.dll AdofaiTweaks
 tar -a -c -f AdofaiTweaks-%version%.zip AdofaiTweaks
 mv AdofaiTweaks-UMM-%version%.zip ..
 cd ..
