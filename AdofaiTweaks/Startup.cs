7295dd4953145be6900f98302549114420a705c1
PizzaLovers007 <trex@parkvue.com>
2022-02-18 20:18:32 -0800

    Fix assembly load order issue (new unity engine)

--- AdofaiTweaks/Startup.cs
+++ AdofaiTweaks/Startup.cs
@@ -1,4 +1,6 @@
-﻿using UnityModManagerNet;
+﻿using System;
+using System.IO;
+using UnityModManagerNet;
 
 namespace AdofaiTweaks
 {
@@ -12,7 +14,24 @@ namespace AdofaiTweaks
         /// </summary>
         /// <param name="modEntry">UMM's mod entry for AdofaiTweaks.</param>
         internal static void Load(UnityModManager.ModEntry modEntry) {
+            LoadAssembly("Mods/AdofaiTweaks/AdofaiTweaks.Strings.dll");
+            LoadAssembly("Mods/AdofaiTweaks/AdofaiTweaks.Translation.dll");
+            LoadAssembly("Mods/AdofaiTweaks/LiteDB.dll");
+
             AdofaiTweaks.Setup(modEntry);
+        }
+
+        /// <summary>
+        /// Loads an assembly at the given file path.
+        /// </summary>
+        /// <param name="path">The path to the assembly.</param>
+        private static void LoadAssembly(string path) {
+            // Load other assemblies
+            using (FileStream stream = new FileStream(path, FileMode.Open)) {
+                byte[] data = new byte[stream.Length];
+                stream.Read(data, 0, data.Length);
+                AppDomain.CurrentDomain.Load(data);
+            }
         }
     }
 }
