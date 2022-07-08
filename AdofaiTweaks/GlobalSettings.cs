489968418e265274fc073d5fee7ae5e1d26eb7cc
CrackThrough <crackthrough@gmail.com>
2022-07-09 06:00:34 +0900

    Oops don't abstract this (target invocation exception)

--- AdofaiTweaks/GlobalSettings.cs
+++ AdofaiTweaks/GlobalSettings.cs
@@ -6,7 +6,7 @@ namespace AdofaiTweaks
     /// <summary>
     /// The global settings for AdofaiTweaks.
     /// </summary>
-    public abstract class GlobalSettings : TweakSettings
+    public class GlobalSettings : TweakSettings
     {
         /// <summary>
         /// The current language for the user.
