3a1e10eb753bebc37b6b983b48bd339332416350
CrackThrough <me@cooki.dev>
2025-02-06 07:15:08 +0900

    feat: editor info tweak

---
+++ AdofaiTweaks/Tweaks/EditorInfo/EditorInfoPatches.cs
@@ -0,0 +1,158 @@
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ADOFAI;
+using ADOFAI.Editor.Actions;
+using AdofaiTweaks.Core.Attributes;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AdofaiTweaks.Tweaks.EditorInfo
+{
+    /// <summary>
+    /// Patches for the Restrict Judgments tweak.
+    /// </summary>
+    internal static class EditorInfoPatches
+    {
+        [SyncTweakSettings]
+        private static EditorInfoSettings Settings { get; set; }
+
+        private static scrFloor lastDisplayedFloor;
+
+        private static readonly FieldInfo EditorLastSelectedFloorField =
+            AccessTools.Field(typeof(scnEditor), "lastSelectedFloor");
+
+        // Twirls, Set Speeds and Holds are already handled by
+        // vanilla CalculateFloorAngleLengths() method
+        private static readonly LevelEventType[] BeatsAlteringLevelEvents =
+            [LevelEventType.FreeRoam, LevelEventType.Pause];
+
+        private static void ResetLastDisplay(bool deactivateText = true) {
+            // Hide the text from last displayed floor
+            if (!lastDisplayedFloor) return;
+            if (!lastDisplayedFloor.editorNumText) return;
+
+            if (deactivateText)
+                lastDisplayedFloor.editorNumText.gameObject.SetActive(false);
+
+            lastDisplayedFloor.editorNumText.letterText.text = lastDisplayedFloor.seqID.ToString();
+            lastDisplayedFloor = null;
+        }
+
+        private static void ForceUpdateFloorDisplay(scnEditor editor, scrFloor displayFloor) {
+            var sb = new StringBuilder();
+
+            var totalAngle = 0d;
+
+            ADOBase.lm.CalculateFloorAngleLengths();
+
+            // Calculate total angle
+            foreach (var floor in editor.selectedFloors) {
+                if (floor.seqID == editor.floors.Count - 1) continue;
+
+                totalAngle += floor.angleLength * Mathf.Rad2Deg;
+
+                var events = editor.events.Where(e =>
+                    BeatsAlteringLevelEvents.Contains(e.eventType) && e.active && e.floor == floor.seqID);
+
+                foreach (var levelEvent in events) {
+                    totalAngle += levelEvent.eventType switch {
+                        LevelEventType.Pause => levelEvent.GetFloat("duration") * 180d,
+                        LevelEventType.FreeRoam => levelEvent.GetInt("duration") * 180d,
+                        _ => 0
+                    };
+                }
+            }
+
+            // Display in a way player wants
+            if (Settings.ShowFloorAngle && totalAngle != 0)
+                sb.AppendLine($"{totalAngle:#.####}\u00b0");
+
+            if (Settings.ShowFloorBeats && totalAngle != 0) {
+                sb.Append($"{totalAngle / 180:#.####}");
+            }
+
+            var text = sb.ToString();
+            if (text.Length == 0) return;
+
+            if (!editor.selectedFloors.Contains(displayFloor))
+                displayFloor = editor.selectedFloors[0];
+
+            displayFloor ??= editor.selectedFloors[^1];
+
+            displayFloor.editorNumText.gameObject.SetActive(true);
+            displayFloor.editorNumText.letterText.text = text;
+
+            // Cache the last displayed floor
+            lastDisplayedFloor = displayFloor;
+        }
+
+        private static void UpdateFloorDisplay(scrFloor displayFloor = null) {
+            var editor = ADOBase.editor;
+            if (!editor) return;
+
+            if (editor.showFloorNums) {
+                ResetLastDisplay(false);
+            } else {
+                ResetLastDisplay();
+
+                if (!displayFloor || !displayFloor.enabled)
+                    displayFloor = editor.selectedFloors[0];
+
+                if (!displayFloor.enabled)
+                    displayFloor = editor.selectedFloors[^1];
+
+                if (!displayFloor.enabled) {
+                    var enabledFloors = editor.selectedFloors.Where(f => f.enabled).ToArray();
+                    if (!enabledFloors.Any()) return;
+
+                    var camera = scrCamera.instance;
+                    var cameraPos = camera
+                        ? camera.transform.position
+                        : Vector3.zero;
+
+                    displayFloor = enabledFloors
+                        .OrderBy(f => Vector3.Distance(f.transform.position.WithZ(0), cameraPos.WithZ(0)))
+                        .First();
+                }
+
+                if (displayFloor.enabled)
+                    ForceUpdateFloorDisplay(editor, displayFloor);
+            }
+        }
+
+        [HarmonyPatch(typeof(scnEditor), "OnSelectedFloorChange")]
+        private static class FloorTextDisplayPatch {
+            private static void Postfix(scrFloor ___lastSelectedFloor) => UpdateFloorDisplay(___lastSelectedFloor);
+        }
+
+        [HarmonyPatch(typeof(scrFloor), "OnBecameInvisible")]
+        private static class PreventFloorTextDisplayDisappearPatch {
+            private static void Prefix() {
+                var editor = ADOBase.editor;
+                if (!editor) return;
+                if (editor.showFloorNums) return;
+                if (editor.playMode) return;
+                if (editor.SelectionIsEmpty()) return;
+
+                UpdateFloorDisplay();
+            }
+        }
+        
+        [HarmonyPatch(typeof(ToggleFloorNumsEditorAction), "Execute")]
+        private static class OnToggleFloorNumsEditorActionPatch {
+            private static void Prefix() {
+                var editor = ADOBase.editor;
+                if (!editor) return;
+
+                if (editor.showFloorNums) {
+                    // Now turning OFF
+                    ResetLastDisplay();
+                } else {
+                    // Now turning ON
+                    ForceUpdateFloorDisplay(editor, lastDisplayedFloor);
+                }
+            }
+        }
+    }
+}
---
+++ AdofaiTweaks/Tweaks/EditorInfo/EditorInfoTweak.cs
@@ -0,0 +1,36 @@
+﻿using AdofaiTweaks.Core;
+using AdofaiTweaks.Core.Attributes;
+using AdofaiTweaks.Strings;
+using UnityEngine;
+
+namespace AdofaiTweaks.Tweaks.EditorInfo
+{
+    /// <summary>
+    /// A tweak for various level editor features.
+    /// </summary>
+    [RegisterTweak(
+        id: "editor_info",
+        settingsType: typeof(EditorInfoSettings),
+        patchesType: typeof(EditorInfoPatches))]
+    internal class EditorInfoTweak : Tweak
+    {
+        /// <inheritdoc/>
+        public override string Name =>
+            TweakStrings.Get(TranslationKeys.EditorInfo.NAME);
+
+        /// <inheritdoc/>
+        public override string Description =>
+            TweakStrings.Get(TranslationKeys.EditorInfo.DESCRIPTION);
+
+        [SyncTweakSettings]
+        private EditorInfoSettings Settings { get; set; }
+
+        /// <inheritdoc/>
+        public override void OnSettingsGUI() {
+            MoreGUILayout.BeginIndent();
+            Settings.ShowFloorAngle = GUILayout.Toggle(Settings.ShowFloorAngle, TweakStrings.Get(TranslationKeys.EditorInfo.SHOW_FLOOR_ANGLE));
+            Settings.ShowFloorBeats = GUILayout.Toggle(Settings.ShowFloorBeats, TweakStrings.Get(TranslationKeys.EditorInfo.SHOW_FLOOR_BEATS));
+            MoreGUILayout.EndIndent();
+        }
+    }
+}
